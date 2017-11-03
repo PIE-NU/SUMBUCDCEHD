d27388f44d0e7ff5750a89937e3da1ae2567676e
alexst0ne <alexanderstone2019@u.northwestern.edu>
2017-11-03 13:57:16 -0500

    Enemy works
    Enemy bounces off walls, beginning in random direction. Fixed some code
    and organized asset folder.

---
+++ src/SUMBUCDCEHD/Assets/Scripts/enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		float xForce = Random.Range (-1f, 1f) * speed;
+		float yForce = Random.Range (-1f, 1f) * speed;
+		Vector2 initialForce = new Vector2 (xForce, yForce);
+		rb.AddForce(initialForce);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
