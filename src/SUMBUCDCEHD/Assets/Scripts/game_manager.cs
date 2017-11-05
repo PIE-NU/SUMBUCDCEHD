85913d178b76f9035311f6dff1bd82cdae3a98c4
LuoLei_Zhao <luoleizhao2018@u.northwestern.edu>
2017-11-05 17:50:02 -0600

    Added increasing difficulty + player resets

---
+++ src/SUMBUCDCEHD/Assets/Scripts/game_manager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class game_manager : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+	float timeSinceLastEnemy = 0f;
+	public float addEnemyTime = 10.0f;
+	public int currentEnemies = 0;
+	public int maxEnemies = 5;
+
+	// Use this for initialization
+	void Start () {
+		resetGame ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeSinceLastEnemy += Time.deltaTime;
+		if (timeSinceLastEnemy > addEnemyTime) {
+			addEnemy ();
+		}
+	}
+
+	public void resetGame() {
+		enemy[] allEnemies = FindObjectsOfType<enemy> ();
+		foreach (enemy e in allEnemies) {
+			Destroy (e.gameObject);
+		}
+		currentEnemies = 0;
+		addEnemy ();
+	}
+
+	void addEnemy() {
+		if (currentEnemies < maxEnemies) {
+			GameObject enemy = Instantiate (enemyPrefab, new Vector3 (0f, 2f, 0f), Quaternion.identity);
+			currentEnemies += 1;
+		}
+		timeSinceLastEnemy = 0f;
+	}
+}
--- src/SUMBUCDCEHD/Assets/Scripts/player_movement.cs
+++ src/SUMBUCDCEHD/Assets/Scripts/player_movement.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class player_movement : MonoBehaviour {
 
+	public Vector3 startLocation;
 	Rigidbody2D m_rigid_body;
 	SpriteRenderer m_sprite_renderer;
 	Vector2 m_left_force;
 	Vector2 m_right_force;
+
+	game_manager gm;
 	// Use this for initialization
 	void Start () {
 		m_rigid_body = GetComponent<Rigidbody2D> ();
 		m_sprite_renderer = GetComponent<SpriteRenderer> ();
 		m_left_force = new Vector2 (-10.0f, 0f);
 		m_right_force = new Vector2 (10.0f, 0f);
+		gm = FindObjectOfType<game_manager> ();
+		resetPlayer ();
 	}
 	
 	// Update is called once per frame
@@ -30,7 +35,13 @@ public class player_movement : MonoBehaviour {
 	virtual internal void OnCollisionEnter2D(Collision2D c ) {
 		if (c.gameObject.GetComponent<enemy> ()) {
 			Debug.Log ("You died!");
-			m_rigid_body.position = new Vector3 (0f, 0f, 0f);
+			resetPlayer ();
+			gm.resetGame ();
 		}
+	}
+
+	void resetPlayer() {
+		m_rigid_body.position = startLocation;
+		m_rigid_body.velocity = new Vector3 (0f, 0f, 0f);
 	}
 }
