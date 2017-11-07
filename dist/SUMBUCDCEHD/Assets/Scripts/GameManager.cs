9eba81bffccf518d21a813ff3883b8ac0fbb1528
Murfalo <murphycangelo@u.northwestern.edu>
2017-11-06 19:04:25 -0600

    Force text serialization

---
+++ dist/SUMBUCDCEHD/Assets/Scripts/GameManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	// Singleton (see: https://en.wikipedia.org/wiki/Singleton_pattern)
+	public static GameManager Instance;
+
+	[SerializeField] private GameObject m_EnemyPrefab;
+
+	// Maximum number of enemies to allow in game at a given time
+	private const int MaxEnemies = 5;
+
+	// Current amount of enemies in the game
+	private int m_CurrentEnemies = 0;
+
+	// Time since the last enemy was spawned
+	private float m_TimeSinceLastEnemy = 0;
+
+	// How frequently an enemy should spawn
+	private float m_AddEnemyTime = 10;
+
+	// Resets the game such that there is only one enemy
+	public void ResetGame()
+	{
+		foreach (var e in FindObjectsOfType<Enemy>())
+		{
+			Destroy(e.gameObject);
+		}
+		m_CurrentEnemies = 0;
+		m_TimeSinceLastEnemy = 0;
+		AddEnemy();
+	}
+
+	// Called upon initialization
+	private void Awake()
+	{
+		// Set the singleton instance
+		if (Instance == null)
+			Instance = this;
+
+		// Reset the game to its initial state
+		ResetGame();
+	}
+
+	// Called once per frame
+	private void Update()
+	{
+		/*
+		 * Increment time since last enemy by the time that has elapsed since
+		 * the last frame.
+		 */
+		m_TimeSinceLastEnemy += Time.deltaTime;
+
+		// If the time since the last enemy is less than the spawn interval, do nothing.
+		if (m_TimeSinceLastEnemy <= m_AddEnemyTime) return;
+
+		// Otherwise, add an enemy and reset the timer!
+		m_TimeSinceLastEnemy = 0;
+		AddEnemy();
+	}
+
+	// Adds an enemy to the game
+	private void AddEnemy()
+	{
+		// If we already have the maximum number of enemies, do nothing.
+		if (m_CurrentEnemies >= MaxEnemies) return;
+
+		// Otherwise, add an enemy to the game.
+		Instantiate(m_EnemyPrefab, new Vector3(0, 2, 0), Quaternion.identity);
+		m_CurrentEnemies += 1;
+	}
+}
---
+++ dist/SUMBUCDCEHD/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{	
+	// Force to add when moving the player in a given direction
+	[SerializeField] private Vector2 m_Force;
+
+	// Starting location of the player
+	[SerializeField] private Vector3 m_StartLocation;
+
+	// Cached components
+	private Rigidbody2D m_RigidBody;
+	private SpriteRenderer m_SpriteRenderer;
+
+	// Initialize cached components and reset player
+	private void Start()
+	{
+		m_RigidBody = GetComponent<Rigidbody2D>();
+		m_SpriteRenderer = GetComponent<SpriteRenderer>();
+		ResetPlayer();
+	}
+
+	// Called once per frame
+	private void Update()
+	{
+		if (Input.GetKey(KeyCode.A))
+		{
+			// Move the player left and update sprite orientation
+			m_RigidBody.AddForce(-m_Force);
+			m_SpriteRenderer.flipX = true;
+		}
+		else if (Input.GetKey(KeyCode.D))
+		{
+			// Move the player right and update sprite orientation
+			m_RigidBody.AddForce(m_Force);
+			m_SpriteRenderer.flipX = false;
+		}
+	}
+
+	// Called when the player's collider overlaps c's collider
+	private void OnCollisionEnter2D(Collision2D c)
+	{
+		// If the other object isn't an enemy, do nothing!
+		if (c.gameObject.GetComponent<Enemy>() == null) return;
+
+		// If it is, the player lost!
+		Debug.Log("You died!");
+		ResetPlayer();
+		GameManager.Instance.ResetGame();
+	}
+
+	// Return the player to its initial state
+	private void ResetPlayer()
+	{
+		m_RigidBody.position = m_StartLocation;
+		m_RigidBody.velocity = new Vector3(0f, 0f, 0f);
+	}
+}
